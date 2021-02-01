65b041ea1d7e7a700b67b2ebee398ec1669b15d5
houweibo-wm <1053064467@qq.com>
2021-02-01 20:56:43 +0800

    say hello

--- R/sayhello.R
+++ R/sayhello.R
@@ -1,6 +1,9 @@
 #' @title a function for say hello
 #' @author webber
 #' @param txt char
+#' @examples
+#' sayhello('hwb')
+#' sayhello('hello,hwb')
 #' @export
 sayhello <- function(txt = "hello"){
   print(txt)
